d366bb4ffbf336c374d2cb22e92bbf23e687fd7a
Nastya-Q <raed.gyfu@gmail.com>
2019-04-13 12:40:49 +0200

    added admin login test

--- java-example/src/test/java/ru/stqa/training/selenium/AdminLoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/AdminLoginTest.java
@@ -3,11 +3,12 @@ package ru.stqa.training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyFirstTest {
+public class AdminLoginTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -19,8 +20,11 @@ public class MyFirstTest {
     }
 
     @Test
-    public void myFirstStep() {
-        driver.get("https://github.com/Nastya-Q/selenium_training");
+    public void loginAsAdmin() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
