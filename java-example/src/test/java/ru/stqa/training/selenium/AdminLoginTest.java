84ca3ece12662e6f1e91fb0ece63a16fa36c8dfb
Nastya-Q <raed.gyfu@gmail.com>
2019-04-14 14:11:48 +0200

    example of setting capabilities for Chrome Browser

--- java-example/src/test/java/ru/stqa/training/selenium/AdminLoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/AdminLoginTest.java
@@ -4,10 +4,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AdminLoginTest {
@@ -17,7 +20,10 @@ public class AdminLoginTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("unexpectedAlertBehaviour", "accept");
+        driver = new ChromeDriver(options);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         //driver = new InternetExplorerDriver();
         //driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 10);
