5e12004cfdebe755dc287e496c8874fbde46c0e2
Nastya-Q <raed.gyfu@gmail.com>
2019-04-28 13:16:46 +0200

    finished Shopcart test: 1) adds product to shopcart and checks counter 2) removes product from shopcart and checks summary table

--- java-example/src/test/java/ru/stqa/training/selenium/ShopCartAddRemoveProductTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/ShopCartAddRemoveProductTest.java
@@ -1,63 +1,60 @@
 package ru.stqa.training.selenium;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-
 import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ShopCartAddRemoveProductTest extends TestBase {
 
     @Test
     public void addAndRemoveProducts() {
-        int productsToAddCount = 2;
+        int productsToAddCount = 3;
+        //add products to the shopcart
         for (int i = 1; i <= productsToAddCount; i++) {
-            driver.get("http://localhost/litecart");
-            driver.findElement(By.className("product")).click();
-            //add product to the shopcart
+            addProductToShopcart();
+            //check that items counter in shopcart was updated
             WebElement productCounter = driver.findElement(By.className("quantity"));
-            // product size selected for products with sizes dropdown list (as yellow duck for example)
-            if (isElementPresent(driver, By.name("options[Size]"))) {
-                Select size = new Select(driver.findElement(By.name("options[Size]")));
-                size.selectByIndex(1);
-            }
-            driver.findElement(By.name("add_cart_product")).click();
-            //wait when shopcart product counter updated
-            String expectedCount = String.valueOf(i);
-            wait.until(ExpectedConditions.attributeToBe(productCounter, "textContent", expectedCount));
-            String count = productCounter.getAttribute("textContent");
+            String expectedCounterValue = String.valueOf(i);
+            wait.until(ExpectedConditions.attributeToBe(productCounter, "textContent", expectedCounterValue));
         }
-
         //remove products from the shopcart
         driver.get("http://localhost/litecart");
         driver.findElement(By.linkText("Checkout »")).click();
-        //until there are elements to remove:
-        while (driver.findElements(By.name("remove_cart_item")).size() > 0) {
-            WebElement checkoutTableBeforeItemRemoval = driver.findElement(By.className("dataTable"));
-            int itemRowsBeforeRemoval = checkoutTableBeforeItemRemoval
-                    .findElements(By.className("item")).size();
-            List<WebElement> elementsToRemove = driver.findElements(By.name("remove_cart_item"));
-            wait.until(ExpectedConditions.elementToBeClickable(elementsToRemove.get(0))).click();
-            //wait until old table disappears
-            wait.until(ExpectedConditions.stalenessOf(checkoutTableBeforeItemRemoval));
-            //if it's not the last element to remove and table should still exist:
-            if (driver.findElements(By.name("remove_cart_item")).size() > 0) {
-                WebElement checkoutTableAfterItemRemoval = driver.findElement(By.className("dataTable"));
-                int itemRowsAfterRemoval = checkoutTableBeforeItemRemoval
-                        .findElements(By.className("item")).size();
-                Assert.assertTrue( (itemRowsBeforeRemoval - itemRowsAfterRemoval) == 1);
-            } else {
-
-            }
-
-            checkoutSummaryTable.getSize();// stale element exception
+        while (driver.findElements(By.name("remove_cart_item")).size() > 0) { //if there are products to remove
+            removeItemFromShopCartAndCheckSummaryTable();
         }
+        //after all products were removed from the shopcart
+        assertTrue(isElementPresent(driver, By.tagName("em")));
+        String finalText = driver.findElement(By.tagName("em")).getAttribute("textContent");
+        assertEquals("There are no items in your cart.", finalText);
+    }
 
-        System.out.println("test");
+    private void removeItemFromShopCartAndCheckSummaryTable() {
+        WebElement checkoutTableBeforeItemRemoval = driver.findElement(By.className("dataTable"));
+        int itemRowsBeforeRemoval = checkoutTableBeforeItemRemoval.findElements(By.className("item")).size();
+        List<WebElement> elementsToRemove = driver.findElements(By.name("remove_cart_item"));
+        wait.until(ExpectedConditions.elementToBeClickable(elementsToRemove.get(0))).click(); //click remove when button is shown
+        wait.until(ExpectedConditions.stalenessOf(checkoutTableBeforeItemRemoval)); //wait until old table disappears
+        if (driver.findElements(By.name("remove_cart_item")).size() > 0) { //if there are products to remove
+            WebElement checkoutTableAfterItemRemoval = driver.findElement(By.className("dataTable"));
+            int itemRowsAfterRemoval = checkoutTableAfterItemRemoval.findElements(By.className("item")).size();
+            assertEquals(1, (itemRowsBeforeRemoval - itemRowsAfterRemoval)); //check that items removed from table
+        }
+    }
 
+    private void addProductToShopcart() {
+        driver.get("http://localhost/litecart");
+        driver.findElement(By.className("product")).click();
+        // if sizes dropdown list is present (e.g. as for yellow duck), some value should be selected
+        if (isElementPresent(driver, By.name("options[Size]"))) {
+            Select size = new Select(driver.findElement(By.name("options[Size]")));
+            size.selectByIndex(1);
+        }
+        driver.findElement(By.name("add_cart_product")).click();
     }
 }
