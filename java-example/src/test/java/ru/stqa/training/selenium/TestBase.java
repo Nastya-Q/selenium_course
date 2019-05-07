8840cf1c1547f5707ccd435607980e65bc827985
Nastya-Q <raed.gyfu@gmail.com>
2019-05-07 14:51:18 +0200

    example of using EventFiringWebDriver

--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -2,21 +2,37 @@ package ru.stqa.training.selenium;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.InvalidSelectorException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class TestBase {
-    public WebDriver driver;
+    public EventFiringWebDriver driver;
     public WebDriverWait wait;
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+    }
 
     boolean areElementsPresent(WebDriver driver, By locator) {
         return driver.findElements(locator).size() > 0;
@@ -36,7 +52,8 @@ public class TestBase {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new MyListener());
         //driver = new FirefoxDriver();
         //driver = new InternetExplorerDriver();
         //not obvious wait for element present
